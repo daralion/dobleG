f6a328da68935c3c8f54dcd7818288efc7d2f3ed
Gabriel <gabrielgedler@gmail.com>
2021-12-13 19:04:44 -0400

    categories now returns list of names

--- Dashboard/DBQuery.py
+++ Dashboard/DBQuery.py
@@ -4,6 +4,10 @@ client = MongoClient("localhost", 27017)
 gobDb = client["GoBDB"]
 jobCollection = gobDb["Jobs"]
 
-categories = jobCollection.find({}, {'attributes.category_name': 1})
+categories = jobCollection.aggregate([
+    {
+        '$project': {'category': 'attributes.category_name'}
+    }
+])
 
 print(list(categories))
