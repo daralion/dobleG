bc61538053dbb11aa83df2a156232c657bc7883e
Gabriel <gabrielgedler@gmail.com>
2021-12-14 18:41:08 -0400

    added list of category counts

--- GoB/dashboard/views.py
+++ GoB/dashboard/views.py
@@ -9,7 +9,13 @@ def home(request):
     categories = DbQuery.get_categories()
     categories_df = pd.DataFrame(categories)
     graph_div = RunGraph.graph_bar(categories_df)
-    context = {'graph_div': graph_div}
+    categories_count = categories_df['category'].value_counts()
+    values_list = categories_count.values.tolist()
+    index_list = categories_count.index.tolist()
+    context = {
+        'graph_div': graph_div,
+        'categories_count': [list(x) for x in zip(index_list, values_list)]
+    }
     return render(request, 'dashboard/home.html', context)
 
 
