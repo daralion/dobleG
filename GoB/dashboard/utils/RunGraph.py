80100992e58cc0dcf9f2d16efe62ef96d6794281
Katherine Gil <kvgill23@gmail.com>
2021-12-15 01:30:07 -0300

    creacion ultima grafica

--- GoB/dashboard/utils/RunGraph.py
+++ GoB/dashboard/utils/RunGraph.py
@@ -97,3 +97,30 @@ def graph_bar2(df,name_category):
         include_plotlyjs=False, output_type='div'
     )
     return text_div
+
+def graph_bar3(df,nombres_features=['category','seniority']):
+    import plotly
+    import plotly.graph_objects as go
+
+    best10=df[nombres_features[0]].value_counts()[:10].index.tolist()
+    df_new=df[df[nombres_features[0]].isin(best10)]
+    df_groupby=df_new.groupby(nombres_features).size()
+
+    df_less_index=df_groupby.reset_index()
+    df_pivot=df_less_index.pivot(index=nombres_features[0],columns=nombres_features[1],values=0)
+    df_pivot.fillna(0,inplace=True)
+    categories=df_pivot.index
+    
+    data=[
+        go.Bar(name='Expert', x=categories, y=df_pivot['Expert'].values),
+        go.Bar(name='Senior', x=categories, y=df_pivot['Senior'].values),
+        go.Bar(name='Semi Senior', x=categories, y=df_pivot['Semi Senior'].values),
+        go.Bar(name='Junior', x=categories, y=df_pivot['Junior'].values),
+        go.Bar(name='Sin experiencia', x=categories, y=df_pivot['Sin experiencia'].values)
+    ]
+
+    text_div=plotly.offline.plot(
+        {'data':data,'layout':go.Layout(xaxis_tickangle=-45,barmode='group')},
+        include_plotlyjs=False, output_type='div'
+    )
+    return text_div
