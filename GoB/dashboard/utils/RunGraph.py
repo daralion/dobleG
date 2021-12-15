a895845e29534c8eec516332ed8f97777fa30135
Katherine Gil <kvgill23@gmail.com>
2021-12-14 22:59:16 -0300

    creacion de nueva grafica

--- GoB/dashboard/utils/RunGraph.py
+++ GoB/dashboard/utils/RunGraph.py
@@ -48,7 +48,6 @@ def graph_pie2(df,name_category,nombres_features='seniority'):
     import plotly
     import plotly.graph_objects as go
 
-    num_in_features=df[nombres_features].value_counts()[:10]
     elim=df['category'].value_counts()[-4:].index.tolist()
     df_new=df[~df['category'].isin(elim)]
     df_groupby=df_new.groupby(nombres_features).size()
@@ -61,6 +60,46 @@ def graph_pie2(df,name_category,nombres_features='seniority'):
 
     text_div=plotly.offline.plot(
         {'data':data,'layout':go.Layout(title=f'Porcentaje de Seniority por {name_category}')},
+        include_plotlyjs=False, output_type='div'
+    )
+    return text_div
+
+def data_manipulation(list_features,name_category):
+    from collections import Counter
+    #Creamos un diccionario con las categorias como llave y una lista vacia como valor
+    dict_new={}
+    for category in list_features:
+        dict_new[category['category']]=[]
+
+    #Llenamos las listas vacias con los tags que pertenecen a cada categoria
+    for category in list_features:
+        dict_new[category['category']]+=[info['attributes']['name'] for info in category['tags']]
+
+    #Esto hara un recuento de los tags para una categoria en especifico
+    dict_count_tags=dict(Counter(dict_new[name_category]))
+
+    #Creacion del dataframe con los recuentos de los tags
+    df_tags=pd.DataFrame([dict_count_tags], index=['Count']).transpose()
+    return df_tags
+
+def graph_bar2(df,name_category):
+    import plotly
+    import plotly.graph_objects as go
+
+    sort_tags=df.sort_values('Count',ascending=False)[:15]
+    
+    sort_tags.values.tolist()
+    list_=[]
+    
+    for l in sort_tags.values.tolist():
+        list_+=l
+        
+    data=[go.Bar(
+        x=sort_tags.index,
+        y=list_
+    )]
+    text_div=plotly.offline.plot(
+        {'data':data,'layout':go.Layout(xaxis_tickangle=-45, title=f'Top 15 de tags para la categoría {name_category}')},
         include_plotlyjs=False, output_type='div'
     )
     return text_div
